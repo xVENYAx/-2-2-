968dbf88515fc799a07f97f05e633666d4570200
t3t3t <t3t3t@DESKTOP-ONP51F3>
2021-09-19 21:22:08 +0300

    Виправлено помилки.

---
+++ Пример 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Пример_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            double p = 3.14159;
+            double x = 2.5;
+            double y = Math.Cos(p * x) / (1 + x * x);
+            Console.WriteLine();
+            Console.WriteLine(" x = {0} \t y = {1} ", x, y);
+        }
+    }
+}
